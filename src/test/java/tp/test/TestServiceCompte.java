5c62abec87f33fbb295483e972f5ff11abd13647
didier <didier@d-defrance.fr>
2019-11-12 11:52:49 +0100

    avec config spring xml

--- src/test/java/tp/test/TestServiceCompte.java
+++ src/test/java/tp/test/TestServiceCompte.java
@@ -3,18 +3,26 @@ package tp.test;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import tp.entity.Compte;
 import tp.service.ServiceCompte;
 
 //test unitaire (JUnit)
 
+//Solutions des Tps sur https://github.com/didier-tp/nova-spring
+
 public class TestServiceCompte {
 	private ServiceCompte serviceCompte; //à tester
 	
 	@Before
 	public void initTest() {
-		//...
+		ApplicationContext contextSpring =
+				new ClassPathXmlApplicationContext("springContext.xml");
+		this.serviceCompte = (ServiceCompte)contextSpring.getBean("serviceCompteImpl"/*id*/);
+		//this.serviceCompte = contextSpring.getBean(ServiceCompte.class);
+		//+.close() ideal dans méthode préfixée par @After ou @AfterClass
 	}
 	
 	@Test
